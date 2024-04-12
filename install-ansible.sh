506bd511f52910bd09e743d2713456a31c5bf770
soda zero <soda.zero.sip@proton.me>
2024-04-12 07:16:46 -0300

    init ubuntu setup

---
+++ install-ansible.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# Works in Ubuntu 22.04 LTS
+# Install pipx
+sudo apt update
+sudo apt install pipx -y
+pipx ensurepath
+
+# Install Ansible
+pipx install --include-deps ansible
+pipx inject ansible argcomplete
