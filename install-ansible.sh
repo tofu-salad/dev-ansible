aff06aae4038fecdb9d5bb1888dd4670ae315ae9
soda zero <soda.zero.sip@proton.me>
2024-09-22 16:50:20 -0300

    ansible install script

--- install-ansible.sh
+++ install-ansible.sh
@@ -3,10 +3,16 @@
 
 # Works in Ubuntu 22.04 LTS
 # Install pipx
-sudo apt update
-sudo apt install pipx -y
-pipx ensurepath
+if   ! command -v pipx > /dev/null ; then
+	echo "pipx not found. installing"
+	sudo apt update
+	sudo apt install pipx -y
+	pipx ensurepath
+	pipx install --include-deps ansible --force
+	pipx inject ansible argcomplete
+
+else 
+	echo "pipx and ansible already installed"
+fi
 
 # Install Ansible
-pipx install --include-deps ansible --force
-pipx inject ansible argcomplete
